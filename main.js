2e0c621de286b38c3ba9d376ad31615cf69498ea
Ammar Bin Nasir <majidammar428@gmail.com>
2020-12-29 03:29:02 +0500

    Improvements to UI

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ app.on('ready', () => {
 
         loadingWindow.close(); // This line must be the last line in the timeout function or every window will be closed (think why, future Ammar might forget)
 
-    }, 1000)
+    }, 4000)
     
     // Replace the default menu tabs on the top
     const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
